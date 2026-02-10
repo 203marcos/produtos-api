7e0390e5df0828ebe8d5129b1d61e7845d755e9f
Marcos <marcosaurelioce@hotmail.com>
2026-02-10 00:28:05 -0300

    feat: implement ProdutoService and register in DI

---
+++ Produtos.Api/Interfaces/IProdutoService.cs
@@ -0,0 +1,13 @@
+using Produtos.Api.DTOs;
+
+namespace Produtos.Api.Interfaces;
+
+public interface IProdutoService
+{
+  Task<IEnumerable<ProdutoDto>> GetAllAsync(string? categoria = null);
+  Task<ProdutoDto?> GetByIdAsync(Guid id);
+  Task<ProdutoDto> CreateAsync(CreateProdutoDto dto);
+  Task<bool> UpdateAsync(Guid id, UpdateProdutoDto dto);
+  Task<bool> DeleteAsync(Guid id);
+  Task<IEnumerable<string>> GetCategoriasAsync();
+}
--- Produtos.Api/Program.cs
+++ Produtos.Api/Program.cs
@@ -1,5 +1,8 @@
+using Produtos.Api.Services;
+using Produtos.Api.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,7 +26,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
